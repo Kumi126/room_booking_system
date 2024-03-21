a2bd388dbd6a8bd401f6cb2425070cb48cfd0ccc
Kumi126 <kumi126927@gmail.com>
2024-03-21 21:41:26 +0000

    add resister property functions

--- __pycache__/sql_app/crud.py
+++ __pycache__/sql_app/crud.py
@@ -1,3 +1,4 @@
+import requests
 from sqlalchemy.orm import Session
 from . import models, schemas ## . means from the same directly
 
@@ -13,3 +14,32 @@ def get_bookings(db: Session, skip:int = 0, limit: int = 100):
     return db.query(models.Booking).offset(skip).limit(limit).all()
 
 
+## resister a user
+def create_user(db: Session, user:schemas.User):
+    db_user = models.User(username=user.username) ## build instance
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+## resister a room
+def create_room(db: Session, room:schemas.Room):
+    db_room = models.Room(room_name=room.room_name, capacity=room.capacity) ## build instance
+    db.add(db_room)
+    db.commit()
+    db.refresh(db_room)
+    return db_room
+
+## resister booking
+def create_booking(db: Session, booking:schemas.Booking):
+    db_booking = models.Booking(
+        room_id = booking.room_id,
+        user_id = booking.user_id,
+        booked_num = booking.booked_num,
+        start_datetime = booking.start_datetime,
+        end_datetime = booking.end_datetime
+    ) ## build instance
+    db.add(db_booking)
+    db.commit()
+    db.refresh(db_booking)
+    return db_booking
